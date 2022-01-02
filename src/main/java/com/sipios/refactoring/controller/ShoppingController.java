a9d32752058dc08f3e47a98e385065962802f0ea
Fred Fabre <frederic@fabre.io>
2022-01-02 08:36:02 +0100

    Commit 1 : - variables renaming - logger deleted because not used

--- src/main/java/com/sipios/refactoring/controller/ShoppingController.java
+++ src/main/java/com/sipios/refactoring/controller/ShoppingController.java
@@ -1,10 +1,5 @@
 package com.sipios.refactoring.controller;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,28 +7,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 @RestController
 @RequestMapping("/shopping")
 public class ShoppingController {
 
-    private Logger logger = LoggerFactory.getLogger(ShoppingController.class);
-
     @PostMapping
-    public String getPrice(@RequestBody Body b) {
-        double p = 0;
-        double d;
+    public String getPrice(@RequestBody Body body) {
+        double price = 0;
+        double discount;
 
         Date date = new Date();
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
-        cal.setTime(date);
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
+        calendar.setTime(date);
 
         // Compute discount for customer
-        if (b.getType().equals("STANDARD_CUSTOMER")) {
-            d = 1;
-        } else if (b.getType().equals("PREMIUM_CUSTOMER")) {
-            d = 0.9;
-        } else if (b.getType().equals("PLATINUM_CUSTOMER")) {
-            d = 0.5;
+        if (body.getType().equals("STANDARD_CUSTOMER")) {
+            discount = 1;
+        } else if (body.getType().equals("PREMIUM_CUSTOMER")) {
+            discount = 0.9;
+        } else if (body.getType().equals("PLATINUM_CUSTOMER")) {
+            discount = 0.5;
         } else {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
@@ -42,78 +39,72 @@ public class ShoppingController {
         // if we are in winter or summer discounts periods
         if (
             !(
-                cal.get(Calendar.DAY_OF_MONTH) < 15 &&
-                cal.get(Calendar.DAY_OF_MONTH) > 5 &&
-                cal.get(Calendar.MONTH) == 5
+                calendar.get(Calendar.DAY_OF_MONTH) < 15 &&
+                    calendar.get(Calendar.DAY_OF_MONTH) > 5 &&
+                    calendar.get(Calendar.MONTH) == 5
             ) &&
-            !(
-                cal.get(Calendar.DAY_OF_MONTH) < 15 &&
-                cal.get(Calendar.DAY_OF_MONTH) > 5 &&
-                cal.get(Calendar.MONTH) == 0
-            )
+                !(
+                    calendar.get(Calendar.DAY_OF_MONTH) < 15 &&
+                        calendar.get(Calendar.DAY_OF_MONTH) > 5 &&
+                        calendar.get(Calendar.MONTH) == 0
+                )
         ) {
-            if (b.getItems() == null) {
+            if (body.getItems() == null) {
                 return "0";
             }
 
-            for (int i = 0; i < b.getItems().length; i++) {
-                Item it = b.getItems()[i];
+            for (int i = 0; i < body.getItems().length; i++) {
+                Item item = body.getItems()[i];
 
-                if (it.getType().equals("TSHIRT")) {
-                    p += 30 * it.getNb() * d;
-                } else if (it.getType().equals("DRESS")) {
-                    p += 50 * it.getNb() * d;
-                } else if (it.getType().equals("JACKET")) {
-                    p += 100 * it.getNb() * d;
+                if (item.getType().equals("TSHIRT")) {
+                    price += 30 * item.getQuantity() * discount;
+                } else if (item.getType().equals("DRESS")) {
+                    price += 50 * item.getQuantity() * discount;
+                } else if (item.getType().equals("JACKET")) {
+                    price += 100 * item.getQuantity() * discount;
                 }
-                // else if (it.getType().equals("SWEATSHIRT")) {
-                //     price += 80 * it.getNb();
-                // }
             }
         } else {
-            if (b.getItems() == null) {
+            if (body.getItems() == null) {
                 return "0";
             }
 
-            for (int i = 0; i < b.getItems().length; i++) {
-                Item it = b.getItems()[i];
+            for (int i = 0; i < body.getItems().length; i++) {
+                Item item = body.getItems()[i];
 
-                if (it.getType().equals("TSHIRT")) {
-                    p += 30 * it.getNb() * d;
-                } else if (it.getType().equals("DRESS")) {
-                    p += 50 * it.getNb() * 0.8 * d;
-                } else if (it.getType().equals("JACKET")) {
-                    p += 100 * it.getNb() * 0.9 * d;
+                if (item.getType().equals("TSHIRT")) {
+                    price += 30 * item.getQuantity() * discount;
+                } else if (item.getType().equals("DRESS")) {
+                    price += 50 * item.getQuantity() * 0.8 * discount;
+                } else if (item.getType().equals("JACKET")) {
+                    price += 100 * item.getQuantity() * 0.9 * discount;
                 }
-                // else if (it.getType().equals("SWEATSHIRT")) {
-                //     price += 80 * it.getNb();
-                // }
             }
         }
 
         try {
-            if (b.getType().equals("STANDARD_CUSTOMER")) {
-                if (p > 200) {
-                    throw new Exception("Price (" + p + ") is too high for standard customer");
+            if (body.getType().equals("STANDARD_CUSTOMER")) {
+                if (price > 200) {
+                    throw new Exception("Price (" + price + ") is too high for standard customer");
                 }
-            } else if (b.getType().equals("PREMIUM_CUSTOMER")) {
-                if (p > 800) {
-                    throw new Exception("Price (" + p + ") is too high for premium customer");
+            } else if (body.getType().equals("PREMIUM_CUSTOMER")) {
+                if (price > 800) {
+                    throw new Exception("Price (" + price + ") is too high for premium customer");
                 }
-            } else if (b.getType().equals("PLATINUM_CUSTOMER")) {
-                if (p > 2000) {
-                    throw new Exception("Price (" + p + ") is too high for platinum customer");
+            } else if (body.getType().equals("PLATINUM_CUSTOMER")) {
+                if (price > 2000) {
+                    throw new Exception("Price (" + price + ") is too high for platinum customer");
                 }
             } else {
-                if (p > 200) {
-                    throw new Exception("Price (" + p + ") is too high for standard customer");
+                if (price > 200) {
+                    throw new Exception("Price (" + price + ") is too high for standard customer");
                 }
             }
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
 
-        return String.valueOf(p);
+        return String.valueOf(price);
     }
 }
 
@@ -122,12 +113,13 @@ class Body {
     private Item[] items;
     private String type;
 
-    public Body(Item[] is, String t) {
-        this.items = is;
-        this.type = t;
+    public Body(Item[] items, String shopperType) {
+        this.items = items;
+        this.type = shopperType;
     }
 
-    public Body() {}
+    public Body() {
+    }
 
     public Item[] getItems() {
         return items;
@@ -149,13 +141,14 @@ class Body {
 class Item {
 
     private String type;
-    private int nb;
+    private int quantity;
 
-    public Item() {}
+    public Item() {
+    }
 
     public Item(String type, int quantity) {
         this.type = type;
-        this.nb = quantity;
+        this.quantity = quantity;
     }
 
     public String getType() {
@@ -166,11 +159,11 @@ class Item {
         this.type = type;
     }
 
-    public int getNb() {
-        return nb;
+    public int getQuantity() {
+        return quantity;
     }
 
-    public void setNb(int nb) {
-        this.nb = nb;
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 }
